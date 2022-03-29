b1d2c73e458afa4017c74af56e8321804a4a4ac8
spideryzarc <spideryzarc@gmail.com>
2022-03-29 10:12:21 -0300

    força bruta

--- src/Queens.java
+++ src/Queens.java
@@ -10,31 +10,37 @@ public class Queens {
         pos = new int[n];
     }
 
-    public int fo(){
+    public int fo() {
         int c = 0;
         //colunas
         for (int i = 0; i < N; i++)
-            for (int j = i+1; j < N; j++)
-                if(pos[i]==pos[j])
+            for (int j = i + 1; j < N; j++)
+                if (pos[i] == pos[j]) {
                     c++;
+                    break;
+                }
         //diagonal \
         for (int i = 0; i < N; i++)
-            for (int j = i+1; j < N; j++)
-                if(i-pos[i]==j-pos[j])
+            for (int j = i + 1; j < N; j++)
+                if (i - pos[i] == j - pos[j]) {
                     c++;
+                    break;
+                }
         //diagonal /
         for (int i = 0; i < N; i++)
-            for (int j = i+1; j < N; j++)
-                if(i+pos[i]==j+pos[j])
+            for (int j = i + 1; j < N; j++)
+                if (i + pos[i] == j + pos[j]) {
                     c++;
+                    break;
+                }
         return c;
     }
 
-    public void print(){
+    public void print() {
         for (int i = 0; i < N; i++) {
             System.out.print("|");
             for (int j = 0; j < N; j++) {
-                if(pos[i]==j)
+                if (pos[i] == j)
                     System.out.print(" x |");
                 else
                     System.out.print("   |");
@@ -43,26 +49,30 @@ public class Queens {
         }
     }
 
-    public void random(int tries){
+    public void random(int tries) {
         int best = Integer.MAX_VALUE;
         int bestPos[] = new int[N];
         for (int i = 0; i < tries; i++) {
             randomize();
             int x = fo();
-            if(x < best){
+            if (x < best) {
                 best = x;
-                if(x==0) {
+                System.arraycopy(pos, 0, bestPos, 0, pos.length);
+                if (x == 0) {
                     System.out.println(i);
                     break;
                 }
-
             }
         }
+        if (best > 0) {
+            System.arraycopy(bestPos, 0, pos, 0, pos.length);
+        }
     }
-    public void randomize(){
+
+    public void randomize() {
         for (int i = 0; i < N; i++)
-            pos[i]=i;
-        for (int i = N-1; i > 0 ; i--) {
+            pos[i] = i;
+        for (int i = N - 1; i > 0; i--) {
             int x = rd.nextInt(i);
             //swap
             int aux = pos[i];
@@ -71,5 +81,36 @@ public class Queens {
         }
 
     }
+    private boolean conflito(int k) {
+        for (int i = 1; i <= k; i++)
+            for (int j = 0; j < i; j++) {
+                //colunas
+                if (pos[i] == pos[j])
+                    return true;
+                //diagonal \
+                if (i - pos[i] == j - pos[j])
+                    return true;
+                //diagonal /
+                if (i + pos[i] == j + pos[j])
+                    return true;
+            }
+        return false;
+    }
+    private boolean coloca(int k){
+        for (int i = 0; i < N; i++) {
+            pos[k]=i;
+            //testar se tem conflito antes de k
 
+            if (coloca(1))
+                break;
+        }
+        return false;
+    }
+    public void forcaBruta(){
+        for (int i = 0; i < N; i++) {
+            pos[0]=i;
+            if (coloca(1))
+                break;
+        }
+    }
 }
