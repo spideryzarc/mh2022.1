d57de940e8ae4c1d5569cde7e9f1dde66c8acc2f
einstein <spideryzarc@gmail.com>
2022-04-11 10:52:41 -0300

    adição do plot svg

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,20 @@
 public class Main {
     public static void main(String args[]) {
-        TSP tsp = new TSP(100);
+        TSP tsp = new TSP(10);
         tsp.randomize();
         System.out.println(tsp);
         Route r = new Route(tsp);
         r.randomize();
         System.out.println(r);
+        View.plot(r,"plot.csv");
 
         r.nearestNeighbor();
         System.out.println(r);
+        View.plot(r,"plot.csv");
 
         r.furtherInsertion();
         System.out.println(r);
+        View.plot(r,"plot.csv");
 
     }
 }
---
+++ src/View.java
@@ -0,0 +1,63 @@
+import jsvg.*;
+
+import java.awt.geom.Point2D;
+import java.io.IOException;
+
+public class View {
+    public static void plot(Route rt, String outFile) {
+        Jsvg svg = new Jsvg();
+        svg.setImageSize(800, 800);
+
+        double minX, minY, maxX, maxY;
+
+        maxX = minX = rt.tsp.pontos.get(0).x;
+        maxY = minY = rt.tsp.pontos.get(0).y;
+        for (Point2D.Double s : rt.tsp.pontos) {
+            if (minX > s.x)
+                minX = s.x;
+            if (minY > s.y)
+                minY = s.y;
+            if (maxX < s.x)
+                maxX = s.x;
+            if (maxY < s.y)
+                maxY = s.y;
+        }
+
+        double r = Math.max(maxX - minX, maxY - minY) / 100;
+
+        svg.vbXmin = (int) (minX - r);
+        svg.vbYmin = (int) (minY - r);
+        svg.vbWidth = (int) (maxX - minX + 3 * r);
+        svg.vbHeight = (int) (maxY - minY + 3 * r);
+        {
+            Point2D.Double a = rt.tsp.pontos.get(rt.v[rt.v.length-1]);
+            Point2D.Double b = rt.tsp.pontos.get(rt.v[0]);
+            svg.add(new Line(a.x, a.y, b.x, b.y, ""));
+            for (int i = 1; i < rt.tsp.N; i++) {
+                a = rt.tsp.pontos.get(rt.v[i - 1]);
+                b = rt.tsp.pontos.get(rt.v[i]);
+                svg.add(new Line(a.x, a.y, b.x, b.y, ""));
+            }
+        }
+        int i = 0;
+        for (Point2D.Double s : rt.tsp.pontos) {
+            svg.add(new Circle(s.x, s.y, r, ""));
+            svg.add(new Text(s.x, s.y, "", "" + i));
+            i++;
+        }
+
+        try {
+            svg.add(new Style("tsp.css"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            svg.save("test.svg");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
