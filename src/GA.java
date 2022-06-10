25ff1666f0385c75e1116544beccaed46a5f6f81
spideryzarc <spideryzarc@gmail.com>
2022-06-10 09:28:50 -0300

    aula 10-06 GA crossovers

--- src/GA.java
+++ src/GA.java
@@ -14,6 +14,7 @@ public class GA implements Solver {
     int K;
     boolean rank;
     TSP tsp;
+    private int map[];
 
     public GA(TSP tsp, int ite, int iniPopSize, int k, boolean rank) {
         this.tsp = tsp;
@@ -22,6 +23,7 @@ public class GA implements Solver {
         K = k;
         this.rank = rank;
         used = new boolean[tsp.N];
+        map = new int[tsp.N];
     }
 
     @Override
@@ -30,7 +32,7 @@ public class GA implements Solver {
         ArrayList<Route> pop = new ArrayList<>();
         ArrayList<Route> os = new ArrayList<>();
         initialize(pop);
-        select(pop,os);
+        select(pop, os);
         bestSol = pop.get(0);
         System.out.println(" GA " + bestSol.cost);
 
@@ -41,6 +43,7 @@ public class GA implements Solver {
                 bestSol.copy(pop.get(0));
                 System.out.println(i + " GA " + bestSol.cost);
             }
+            System.out.println(pop.get(pop.size()-1).cost);
         }
         assert Utils.equals(bestSol.cost, tsp.cost(bestSol.v)) : "variável 'cost' está inconsistente";
         runTime = (int) (System.currentTimeMillis() - t);
@@ -57,7 +60,7 @@ public class GA implements Solver {
         pop.clear();
         if (rank) {
             Collections.sort(os, (a, b) -> Double.compare(a.cost, b.cost));
-            pop.addAll(os.subList(0,Math.min(K,os.size())));
+            pop.addAll(os.subList(0, Math.min(K, os.size())));
         } else {//torneio
 
         }
@@ -65,23 +68,43 @@ public class GA implements Solver {
 
     }
 
-    private void offspring(ArrayList<Route> os, ArrayList<Route> pop) {
+    private void offspring(ArrayList<Route> os, ArrayList<Route> elite) {
         os.clear();
-        for (int i = 1; i < pop.size(); i++)
+        for (int i = 1; i < elite.size(); i++)
             for (int j = 0; j < i; j++) {
-                Route r = crossover(pop.get(i), pop.get(j));
-                os.add(r);
-                r = crossover(pop.get(j), pop.get(i));
-                os.add(r);
+                Route a = elite.get(i);
+                Route b = elite.get(j);
+                os.add(twoPointsCrossover(a, b));
+                os.add(twoPointsCrossover(b, a));
+                os.add(orderCrossover(a, b));
+                os.add(orderCrossover(b, a));
+                os.add(partiallyMappedCrossover(a, b));
+                os.add(partiallyMappedCrossover(b, a));
             }
     }
 
     private boolean used[];
 
-    private Route crossover(Route A, Route B) {
+    /**
+     * Two points crossover
+     *
+     * @param A a solution
+     * @param B a solution
+     * @return recombination (crossover) of A and B
+     */
+    private Route twoPointsCrossover(Route A, Route B) {
+        //seleciona dois pontos de corte
+        int a = Utils.rd.nextInt(tsp.N);
+        int b = Utils.rd.nextInt(tsp.N);
+        while (a == b)
+            b = Utils.rd.nextInt(tsp.N);
+        if (a > b) {
+            int aux = a;
+            a = b;
+            b = aux;
+        }
+
         Route C = new Route(tsp);
-        int a = tsp.N / 3;
-        int b = a * 2;
         Arrays.fill(used, false);
         Arrays.fill(C.v, -1);
         for (int i = 0; i < a; i++) {
@@ -107,6 +130,112 @@ public class GA implements Solver {
                         break;
                     }
             }
+        C.cost = tsp.cost(C.v);
+        return C;
+    }
+
+    /**
+     * order crossover
+     *
+     * @param A a solution
+     * @param B a solution
+     * @return recombination (crossover) of A and B
+     */
+    private Route orderCrossover(Route A, Route B) {
+        //seleciona dois pontos de corte
+        int a = Utils.rd.nextInt(tsp.N);
+        int b = Utils.rd.nextInt(tsp.N);
+        while (a == b)
+            b = Utils.rd.nextInt(tsp.N);
+        if (a > b) {
+            int aux = a;
+            a = b;
+            b = aux;
+        }
+
+        Route C = new Route(tsp);
+        Arrays.fill(used, false);
+        Arrays.fill(C.v, -1);
+        for (int i = a; i < b; i++) {
+            C.v[i] = B.v[i];
+            used[C.v[i]] = true;
+        }
+        int j = b;
+        for (int i = b; i < tsp.N; i++) {
+            if (!used[A.v[i]]) {
+                C.v[j] = A.v[i];
+                used[C.v[j]] = true;
+                j++;
+                if (j == tsp.N)
+                    j = 0;
+            }
+        }
+        for (int i = 0; i < b; i++) {
+            if (!used[A.v[i]]) {
+                C.v[j] = A.v[i];
+                used[C.v[j]] = true;
+                j++;
+                if (j == tsp.N)
+                    j = 0;
+                if (j == a)
+                    break;
+            }
+        }
+
+        Utils.rollZero(C.v);
+        C.cost = tsp.cost(C.v);
+        return C;
+    }
+
+    /**
+     * Partially mapped crossover
+     *
+     * @param A a solution
+     * @param B a solution
+     * @return recombination (crossover) of A and B
+     */
+    private Route partiallyMappedCrossover(Route A, Route B) {
+        //seleciona dois pontos de corte
+        int a = Utils.rd.nextInt(tsp.N);
+        int b = Utils.rd.nextInt(tsp.N);
+        while (a == b)
+            b = Utils.rd.nextInt(tsp.N);
+        if (a > b) {
+            int aux = a;
+            a = b;
+            b = aux;
+        }
+
+        Route C = new Route(tsp);
+        Arrays.fill(used, false);
+        Arrays.fill(C.v, -1);
+//        Arrays.fill(map, -1);//não precisa, só por segurança
+        //copiar centro
+        for (int i = a; i < b; i++) {
+            C.v[i] = B.v[i];
+            used[C.v[i]] = true;
+            map[B.v[i]] = A.v[i];
+        }
+
+        //copiar extremidades
+        for (int i = 0; i < tsp.N; i++) {
+            if (!used[A.v[i]] && C.v[i] == -1) {
+                C.v[i] = A.v[i];
+                used[C.v[i]] = true;
+            }
+        }
+
+        //fechar permutação usando o mapa
+        for (int i = 0; i < tsp.N; i++) {
+            if (C.v[i] == -1) {
+                int x = map[A.v[i]];
+                while (used[x])
+                    x = map[x];
+                C.v[i] = x;
+                used[x] = true;
+            }
+        }
+        Utils.rollZero(C.v);
         C.cost = tsp.cost(C.v);
         return C;
     }
